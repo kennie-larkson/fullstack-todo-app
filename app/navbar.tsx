9a2897c74b61250a037946c09778ac6ea866fe96
kennie-larkson <materialkenny@gmail.com>
2024-10-15 12:08:23 +0100

    navbar feature

---
+++ app/navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+
+export default function Navbar() {
+  const [isLoggedin, setIsLoggedIn] = React.useState<boolean>(true);
+  return (
+    <div className="flex items-center justify-center md:justify-around space-x-4  py-3 md:px-12 bg-blue-500 border-0 sticky top-0 z-50 ">
+      <div className="flex">
+        <ul className="flex items-center justify-start md:justify-around space-x-2 md:space-x-8">
+          <li className="flex cursor-pointer  md:hover:font-semibold">Home</li>
+          <li className="flex cursor-pointer  md:hover:font-semibold">About</li>
+          <li className="flex cursor-pointer  md:hover:font-semibold">
+            Subscription
+          </li>
+        </ul>
+      </div>
+      <div
+        className={` ${
+          isLoggedin ? "hidden" : "flex"
+        } cursor-pointer md:hover:font-semibold`}
+      >
+        <p>Signup</p>
+      </div>
+      <div
+        className={` ${
+          isLoggedin ? "flex" : "hidden"
+        } cursor-pointer md:hover:font-semibold`}
+      >
+        <p>Sign Out</p>
+      </div>
+    </div>
+  );
+}
