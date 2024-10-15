6dd8f530d3a4e5bfe6098df1fdae0b1ad15ed0bc
kennie-larkson <materialkenny@gmail.com>
2024-10-15 11:13:10 +0100

    editted spelling error on SearchInput component placeholder

--- app/searchInput.tsx
+++ app/searchInput.tsx
@@ -6,7 +6,7 @@ export default function SearchInput() {
         name="search-box"
         id="search-box"
         className="flex px-4 py-2 rounded-full w-full md:w-3/5 bg-slate-200 font-normal"
-        placeholder="Search your ativities here..."
+        placeholder="Search your activity list here..."
       />
     </>
   );
